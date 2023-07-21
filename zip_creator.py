a1923587a1d31f1c58e31e440c23b8c72d77314d
Alex <a.bajew@medialex.online>
2023-07-21 11:41:52 +0200

    added zip_creator.make_zip() function

---
+++ zip_creator.py
@@ -0,0 +1,8 @@
+import zipfile
+import pathlib
+def make_zip(filepath, dest_dir, zip_name):
+    dest_dir = pathlib.Path(dest_dir, zip_name)
+    with zipfile.ZipFile(dest_dir + , "w") as zip_it:
+        for file in filepath:
+            zip_it.write(file)
+
--- zipper.py
+++ zipper.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as gui
-
+import zip_creator
 
 
 #------------- GUI --------------
